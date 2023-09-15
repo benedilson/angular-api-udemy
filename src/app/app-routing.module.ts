e0ff57cf3af9c93f4e5a1639cf452289729f2d88
Benedilson <noslideneb@gmail.com>
2023-09-15 11:18:35 -0300

    Utilizando lazzy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'usuario', component: UsuarioComponent},
+  {path: 'home', loadChildren:() => import('./home/home.module').then((m) => m.HomeModule)},
+  {path: 'usuario', loadChildren:() => import('./usuario/usuario.module').then((u) => u.UsuarioModule)},
   {path: '', pathMatch:'full', redirectTo:'/home'}
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash:true})
   ],
   exports: [RouterModule]
 })
